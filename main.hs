e04f8abdd736b2c56fae3f47e8e6c91186b04483
marcosblandim <marcosblandim@gmail.com>
2019-11-19 10:40:42 -0300

    .

--- main.hs
+++ main.hs
@@ -1,27 +1,4 @@
--- Nao to conseguindo receber uma lista como input.
--- Por enquanto, o input é digitado na linha de codigo mesmo :(
-
--- Printar o tabuleiro formatado tambem nao ta rolando.
-
--- Como printar o segundo termo da lista:
-{-
-    lista = [1,2,3,4,5]
-    print lista !! 1
-
--}
-
-module Main where
-
-dica1 = tabuleiro
-
-main = do
-    
-    -- Declarando e inicializando tabuleiro.
-    --tabuleiro :: [[Int,Int,Int,Int,Int,Int,Int,Int],[Int,Int,Int,Int,Int,Int,Int,Int],[Int,Int,Int,Int,Int,Int,Int,Int],[Int,Int,Int,Int,Int,Int,Int,Int],[Int,Int,Int,Int,Int,Int,Int,Int],[Int,Int,Int,Int,Int,Int,Int,Int],[Int,Int,Int,Int,Int,Int,Int,Int],[Int,Int,Int,Int,Int,Int,Int,Int]]
-    let tabuleiro = [[-1,-1,-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1,-1,-1]]
-    
-    -- Preencher tabuleiro com dica 1
-    tabuleiro <- dica1 tablueiro
-
-    -- Printar tabuleiro resolvido.
-    print tabuleiro
+SolvePuzzle :: [[Int]] -> [[Int]]
+SolvePuzzle(x:xs) = ((dica1 x)):(SolvePuzzle xs))
+    where dica1 (x:xs) = -- if x == head xs -> ocorrencia da dica1
+        
