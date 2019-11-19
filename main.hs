ec04a46d6eb3785cf05e9f3032fffd52903af5b4
marcosblandim <marcosblandim@gmail.com>
2019-11-19 15:33:40 -0300

    .

--- main.hs
+++ main.hs
@@ -1,4 +1,15 @@
-SolvePuzzle :: [[Int]] -> [[Int]]
-SolvePuzzle(x:xs) = ((dica1 x)):(SolvePuzzle xs))
-    where dica1 (x:xs) = -- if x == head xs -> ocorrencia da dica1
+--import Control.Lens
+
+replace' :: Int -> Int -> [Int] -> [Int]
+replace' _ _ [] = []
+replace' n novoValor (x:xs) = if n == 0
+                                then (novoValor:xs)
+                            else (x:(replace' (n-1) novoValor xs))
+
+dica1 :: [[Int]] -> [[Int]]
+dica1(x:xs) = ((dica1Linha x)):(dica1 xs))
+    where dica1Linha (x:xs) = if (fst xs == snd xs) && x == -1
+                                then if 
+        
+        -- if x == head xs -> ocorrencia da dica1
         
